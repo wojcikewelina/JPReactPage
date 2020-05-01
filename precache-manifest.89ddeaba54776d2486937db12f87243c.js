00bd66a86955a2292d7a5b1747472f0c49a2dcc0
wojcikewelina <gargula.ewe@gmail.com>
2020-05-01 11:32:00 +0200

    Updates

---
+++ precache-manifest.89ddeaba54776d2486937db12f87243c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b40d72bcaccc79516c63006e6c0258",
+    "url": "/JPReactPage/index.html"
+  },
+  {
+    "revision": "a24f594c0c68c4f83863",
+    "url": "/JPReactPage/static/css/main.b6e06fa1.chunk.css"
+  },
+  {
+    "revision": "dd456a06c402fb6612c9",
+    "url": "/JPReactPage/static/js/2.731b53c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JPReactPage/static/js/2.731b53c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24f594c0c68c4f83863",
+    "url": "/JPReactPage/static/js/main.da4edf85.chunk.js"
+  },
+  {
+    "revision": "3aafc798043ebe70ba74",
+    "url": "/JPReactPage/static/js/runtime-main.30babe5c.js"
+  }
+]);
